8b1a15e847b5a1506c6f67cac4397a878fae3cbb
0xdaydream <0xdaydream@proton.me>
2023-12-03 19:37:20 +0100

    Solve Day 02, Part 1

---
+++ day02/part1.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"slices"
+	"strconv"
+)
+
+const (
+	redCubes   = 12
+	greenCubes = 13
+	blueCubes  = 14
+)
+
+func main() {
+	greenRegex := regexp.MustCompile(`(\d+) green`)
+	redRegex := regexp.MustCompile(`(\d+) red`)
+	blueRegex := regexp.MustCompile(`(\d+) blue`)
+
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var sum int
+	for gameID := 1; scanner.Scan(); gameID++ {
+		line := scanner.Text()
+
+		greenNumbers := findNumbersRegex(greenRegex, line)
+		redNumbers := findNumbersRegex(redRegex, line)
+		blueNumbers := findNumbersRegex(blueRegex, line)
+
+		maxGreen := slices.Max(greenNumbers)
+		maxRed := slices.Max(redNumbers)
+		maxBlue := slices.Max(blueNumbers)
+
+		if maxRed <= redCubes && maxGreen <= greenCubes && maxBlue <= blueCubes {
+			sum += gameID
+		}
+	}
+	fmt.Println(sum)
+}
+
+// findNumbersRegex takes a regex pattern and a line of text
+// (e.g., "Game1: 3 blue...") and returns a slice of all numbers
+// associated with the color that regex matches
+func findNumbersRegex(regex *regexp.Regexp, line string) []int {
+	var numbers []int
+	matches := regex.FindAllStringSubmatch(line, -1)
+	for _, match := range matches {
+		matchInt, _ := strconv.Atoi(match[1])
+		numbers = append(numbers, matchInt)
+	}
+	return numbers
+}
